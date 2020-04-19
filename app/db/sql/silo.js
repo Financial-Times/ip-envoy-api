bef5262aaa5b1c142372f2ad6bc3c250d268d708
Ben Fletcher <bjfletcher@gmail.com>
2020-04-19 20:29:35 +0100

    add siloType details to each silo 🐿 v2.12.5

--- app/db/sql/silo.js
+++ app/db/sql/silo.js
@@ -4,6 +4,8 @@ async function list({ entityType, journeyId }) {
   const knex = connect(entityType);
   let query = `
     SELECT * from core.silo
+    LEFT JOIN core."siloType"
+    ON silo."siloTypeId" = "siloType"."siloTypeId"
   `;
   if (journeyId) {
     query += `
