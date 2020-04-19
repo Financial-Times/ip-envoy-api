00b45d2f67707f56bf02d4c83225cc3cd00be096
Ben Fletcher <bjfletcher@gmail.com>
2020-04-19 21:30:20 +0100

    clearer column names with the table joins 🐿 v2.12.5

--- app/db/sql/action.js
+++ app/db/sql/action.js
@@ -3,11 +3,11 @@ const { connect } = require("../connect");
 async function list({ entityType }) {
   const knex = connect(entityType);
   let query = `
-    SELECT * from core.action_silo
-    LEFT JOIN core.action
-    ON action_silo."actionId" = action."actionId"
-    LEFT JOIN core."actionType"
-    ON action."actionTypeId" = "actionType"."actionTypeId"
+    SELECT * from core.action_silo s
+    LEFT JOIN (SELECT "actionId", "actionTypeId", name as "actionName" from core.action) a
+    ON s."actionId" = a."actionId"
+    LEFT JOIN (SELECT "actionTypeId", name as "actionTypeName" from core."actionType") at
+    ON a."actionTypeId" = at."actionTypeId"
   `;
   const res = await knex.raw(query);
   return res.rows;
--- app/db/sql/silo.js
+++ app/db/sql/silo.js
@@ -3,9 +3,9 @@ const { connect } = require("../connect");
 async function list({ entityType, journeyId }) {
   const knex = connect(entityType);
   let query = `
-    SELECT * from core.silo
-    LEFT JOIN core."siloType"
-    ON silo."siloTypeId" = "siloType"."siloTypeId"
+    SELECT * from core.silo s
+    LEFT JOIN (SELECT "siloTypeId", name as "siloTypeName" from core."siloType") st
+    ON s."siloTypeId" = st."siloTypeId"
   `;
   if (journeyId) {
     query += `
